a1c676f5140de0acbb873889dc75caa7f7a6d270
sako kazuhiro <sako@rio.ne.jp>
2019-12-16 05:37:30 +0900

    s

--- app/models/admin_user.rb
+++
@@ -1,6 +0,0 @@
-class AdminUser < ApplicationRecord
-  has_secure_password
-
-  validates :name, :password_digest, presence: true
-  validates :name, uniqueness: true
-end
--- app/models/news.rb
+++
@@ -1,4 +0,0 @@
-class News < ApplicationRecord
-  has_many :news_contents, dependent: :destroy, inverse_of: :news
-  accepts_nested_attributes_for :news_contents, allow_destroy: true
-end
